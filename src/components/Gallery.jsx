52e57be5a313902806c057ae5d73aaa981b437c2
Luigi Iossa <luigi_iossa@hotmail.com>
2023-07-20 12:54:02 +0200

    immagini

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -8,29 +8,29 @@ function Gallery() {
 
                     <Col lg={4} md={12} className='mb-3 mb-lg-0'>
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/356212630_705015394971143_1321515767199736782_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=u3TSbcxUXg8AX8MWGVu&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfClQCExdmaGdxcz4wKlquXvU_RrF31amNKiP0tQNsyDaw&oe=64B28FB4'
+                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/361579207_720207913451891_1639500093106295419_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=otQDAlGDnzMAX_C9hH4&_nc_oc=AQkYiAJqP0_f4u7YY-omHSVO6Mgcdf39sUgf76mdrSyXuW3OpjxglJ8GgM9nSsAxQjbNkE6FBtu9b2ts0yNHQUHs&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfDmT7myabH1LobO-lrVenNcUp57q-NfvolgzDE7O-Y4OA&oe=64BD53E7'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/354244188_698604255612257_5978695952644064026_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=8Zsj-eM1zegAX9msFN4&_nc_oc=AQkzpDtdczyhkKuqTAnJd84WExlZ1NMcVtTaQh_yitEqJKPupfbsG2hQ3pkJn2FN4hsvfWQgUmZ1cvNKB9xwAY0a&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfAbhYiraJviuv6cgebRnDDb70KcghgbPuclVTMgJbQtYA&oe=64B2FD5D'
+                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/361652176_720691220070227_7051201895903067108_n.jpg?stp=dst-jpg_s1080x2048&_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=60elgijyBY0AX9SQfjA&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfBfER8pX5p0ZplDxJpl4qnNfEEWKQlx_c4re7YQgEiGEA&oe=64BE5EB2'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
                             src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t1.6435-9/56786185_2328802217183404_4649429954801434624_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a26aad&_nc_ohc=HVYmG_JJOrAAX9o-cFD&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfDLL4D-f0Cy_gCGQJFDwHTGfijGCqxZrGQPR6lo_AV4Ow&oe=64D5FD70'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/347427779_3506789992923528_6245396072715895559_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=7fGe5t5IckAAX81Zj5T&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfDXDrwCd_pQsIwenEPsjtzROoM7XyoXV1ZHIoZdCmgmow&oe=64B46888'
+                            src='https://www.swahilimodern.com/cdn/shop/files/sen84q_90q_107q_ls2_1024x1024.jpg?v=1685559843'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/216989021_4310720555658217_8726271223579893709_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_ohc=SrakOTsRSxMAX8J6kJo&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfBRiCKoQ5zD5JgdYlHJrN7-iZunGV95H98PTqVjL-1qVA&oe=64B345B2'
+                            src='https://www.swahilimodern.com/cdn/shop/files/sen90r_ls_1024x1024.jpg?v=1685561353'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
                     </Col>
 
                     <Col lg={4} className='mb-3 mb-lg-0'>
@@ -38,54 +38,55 @@ function Gallery() {
                         <img
                             src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t31.18172-8/30072885_1829448603785437_3660643955365167182_o.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_ohc=83BxRFLSoF4AX9GscHP&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfCl73rgW1NvI8jUBZj19lpLU5NAiBvVgUPHOOAg1vsqIA&oe=64D5EB86'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/349614071_1442908516515043_1949665143873756754_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_ohc=yn_tN1mB_3wAX_Us7yy&_nc_oc=AQk3eJ73c9rbxnFiGLsGf1FeJvY6QWbiVu2pPgnSK0_UIxfSrYdqqbn09v0_YiubFVTo0rq22nVnB9fWZJ1afYGi&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfANj7pWg2bdHkuerdR9HAku_MkG3c-E2vxDPJCDOeQJNg&oe=64B43DEB'
+                            src='https://www.swahilimodern.com/cdn/shop/products/sen45c_5_1024x1024.jpg?v=1647452630'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/352209624_796676111773668_1804910009344032827_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=k1Y33zU-QO0AX8ppYO-&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfBEyuSWp2cYgQD2-lvkA6mZaethwIEuHGN0DcpblWl6fA&oe=64B36451'
+                            src='https://www.swahilimodern.com/cdn/shop/products/sen45c_2_1024x1024.jpg?v=1647452630'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
                             src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t1.18169-9/13133341_1161609143902723_9128528176846769816_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=174925&_nc_ohc=czW4DTJXaFgAX94b3Rr&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfBY-WlsJ3jk4egG0NbQcrRvzZ8eoniqcIZDDttlRDv1Ig&oe=64D5EA98'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
+
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/336478819_2233295610348825_5419825314569413120_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=BmU4mZtCC2wAX-a-KS4&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfBTeiRChhTAFzULwQ2_mMfCaWAe0hnfI3iDckVKR9DrBg&oe=64B43BD7'
+                            src='https://www.swahilimodern.com/cdn/shop/products/SEN84N_ls02_1024x1024.jpg?v=1647557119'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
                     </Col>
 
                     <Col lg={4} className='mb-4 mb-lg-0'>
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/336334227_3412502112326200_6920485107708096646_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=0lzivzdXAJQAX8ljNBZ&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfC4gpd0hS8Lo3qPOjKSBiFKox5-X9mqIcmwr55sl2tRZA&oe=64B3DC21'
+                            src='https://www.swahilimodern.com/cdn/shop/products/sen15i_4_1024x1024.jpg?v=1654721316'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/348428198_779689276933847_8131065158000390629_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=X1VTJPJfUloAX9WBaDd&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfCyD-64-zXJMHsbaM9MAjLAkIpH09bY8_cCioAfWA8V9A&oe=64B37577'
+                            src='https://www.swahilimodern.com/cdn/shop/products/sen13d_5_1024x1024.jpg?v=1654720418'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
-
-                        <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/334281600_526943659509776_6630722299146455191_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=vn3ZrNDjiVgAX-k2wRb&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfCU7T77KaHEieYPKrXwWZa94b8XbGQ86yewOx_R1pVQ7g&oe=64B2D913'
-                            className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
                             src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t1.6435-9/203301343_4249954585068148_3955030548815103912_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a26aad&_nc_ohc=wfU4CUEtgmYAX_MslLQ&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfAoplrFjOXjXkk6F0vuES9P-ZykgtYInd5sOKSGZ-FWMQ&oe=64D5EFC9'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
 
                         <img
-                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/334126940_1159373681419608_7129872359389049039_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=FrllkY23tRsAX_jL9Mf&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfDjphffjS6nt6g32P6MWKQkN7VyYR5T2KZYOeI4lHso5A&oe=64B2B877'
+                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/348430234_554407143434234_2344822526152586429_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_ohc=saqvda1YgB4AX_EChZf&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfDYZEcBXzZRjibX4Y14oHhiB4fSEaqNI4Lb6FnX2nGUOw&oe=64BDB152'
                             className='w-100 mb-3'
-                            alt='african basket made in Senegal'/>
+                            alt='african basket made in Senegal' />
+
+                        <img
+                            src='https://scontent.fmxp6-1.fna.fbcdn.net/v/t39.30808-6/352209624_796676111773668_1804910009344032827_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_ohc=jIyNOwbUMRUAX_xBshE&_nc_oc=AQncTCQY6Y9UZy_ZK1Nepx_Cou_q2yr3YDqqmjgJapumKk55aP-Ooy1HpqB6gHyzzKD-QgwYdfXlbZbcMTnmONO6&_nc_ht=scontent.fmxp6-1.fna&oh=00_AfCUA2pYNDUdGGAJ6C1qorpixFxLqejlpKiawVCeihD_bw&oe=64BD4791'
+                            className='w-100 mb-3'
+                            alt='african basket made in Senegal' />
                     </Col>
                 </Row>
             </Container>
