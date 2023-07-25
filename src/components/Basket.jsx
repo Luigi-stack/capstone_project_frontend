285c2f48912b9de435305f1b40683d37931121c6
Luigi Iossa <luigi_iossa@hotmail.com>
2023-07-25 19:16:07 +0200

    cuori

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -3,10 +3,13 @@ import { Container, Row, Col } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
+
 function Basket() {
   const [baskets, setBaskets] = useState([]);
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [favoriteItems, setFavoriteItems] = useState([]);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -34,17 +37,26 @@ function Basket() {
   useEffect(() => {
     const savedCartItems = JSON.parse(localStorage.getItem("cartItems"));
     const savedTotalPrice = JSON.parse(localStorage.getItem("totalPrice"));
+    const savedFavoriteItems = JSON.parse(localStorage.getItem("favoriteItems"));
+
 
     if (savedCartItems && savedTotalPrice) {
       setCartItems(savedCartItems);
       setTotalPrice(savedTotalPrice);
     }
+
+    if (savedFavoriteItems) {
+      setFavoriteItems(savedFavoriteItems);
+    }
+
   }, []);
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
     localStorage.setItem("totalPrice", JSON.stringify(totalPrice));
-  }, [cartItems, totalPrice]);
+    localStorage.setItem("favoriteItems", JSON.stringify(favoriteItems));
+
+  }, [cartItems, totalPrice, favoriteItems]);
 
   const addToCart = (basket) => {
     const existingItem = cartItems.find((item) => item.id === basket.id);
@@ -95,6 +107,22 @@ function Basket() {
     console.log("Proceed to checkout!");
   };
 
+  const toggleFavorite = (basket) => {
+    if (favoriteItems.some((item) => item.id === basket.id)) {
+      setFavoriteItems(favoriteItems.filter((item) => item.id !== basket.id));
+    } else {
+      setFavoriteItems([...favoriteItems, basket]);
+    }
+  };
+
+  const removeFromFavorites = (item) => {
+    setFavoriteItems((prevFavorites) =>
+      prevFavorites.filter((favItem) => favItem.id !== item.id)
+    );
+  };
+
+
+
   return (
     <Container>
       <Row>
@@ -105,10 +133,7 @@ function Basket() {
             className="d-flex justify-content-center mt-3"
             key={basket.id}
           >
-            <Card
-              className="rounded-0"
-              style={{ width: "18rem", margin: "10px" }}
-            >
+            <Card className="rounded-0" style={{ width: "18rem", margin: "10px" }}>
               <Card.Body>
                 <Card.Img src={basket.image} />
                 <Card.Title>{basket.name}</Card.Title>
@@ -118,12 +143,25 @@ function Basket() {
                   Price: {basket.price}€
                 </Card.Text>
               </Card.Body>
-              <Button
-                className="btn btn-light rounded-0"
-                onClick={() => addToCart(basket)}
-              >
-                Add to Cart
-              </Button>
+              <div className="d-flex align-items-center justify-content-between">
+                <Button
+                  className="btn btn-light rounded-0 flex-grow-1"
+                  onClick={() => addToCart(basket)}
+                >
+                  Add to Cart
+                </Button>
+                {/* Bottone per gestire l'aggiunta e la rimozione dai preferiti */}
+                <Button
+                  className="btn btn-light rounded-0"
+                  onClick={() => toggleFavorite(basket)}
+                >
+                  {favoriteItems.some((item) => item.id === basket.id) ? (
+                    <i className="bi bi-heart-fill text-danger"></i>
+                  ) : (
+                    <i className="bi bi-heart"></i>
+                  )}
+                </Button>
+              </div>
             </Card>
           </Col>
         ))}
@@ -136,24 +174,41 @@ function Basket() {
         ) : (
           <>
             {cartItems.map((item) => (
-              <>
-                <Container
-                  className="d-flex align-items-center justify-content-between"
-                  key={item.id}
+              <Container
+                className="d-flex align-items-center justify-content-between"
+                key={item.id}
+              >
+                <p>
+                  {item.name} ({item.quantity}x) {item.price}€
+                  {/* Controllo per verificare se l'elemento è nei preferiti */}
+                  {favoriteItems.some((favItem) => favItem.id === item.id) ? (
+                    <span
+                      className="ms-2 text-danger"
+                      title="Remove from Favorites"
+                      onClick={() => removeFromFavorites(item)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <i className="bi bi-heart-fill text-danger"></i>
+                    </span>
+                  ) : (
+                    <span
+                      className="ms-2 text-primary"
+                      title="Add to Favorites"
+                      onClick={() => toggleFavorite(item)}
+                      style={{ cursor: "pointer" }}
+                    >
+                      <i className="bi bi-heart text-dark"></i>
+                    </span>
+                  )}
+                </p>
+                <img src={item.image} alt="" width={50} />
+                <Button
+                  className="btn btn-danger rounded-0"
+                  onClick={() => removeFromCart(item)}
                 >
-                  <p>
-                    {item.name} ({item.quantity}x) {item.price}€
-                  </p>
-                  <img src={item.image} alt="" width={50} />
-                  <Button
-                    className="btn btn-danger rounded-0"
-                    onClick={() => removeFromCart(item)}
-                  >
-                    Remove
-                  </Button>
-                </Container>
-                <hr />
-              </>
+                  Remove from Cart
+                </Button>
+              </Container>
             ))}
             <p className="fs-4">Total Price: {totalPrice}€</p>
             <Button
@@ -167,6 +222,7 @@ function Basket() {
         )}
       </Row>
     </Container>
+
   );
 }
 
